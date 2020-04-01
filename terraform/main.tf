a9a9591f2cc2093fffdf18cc316e659485b41c6b
souljapanic <souljapanic@gmail.com>
2020-04-01 17:02:03 +0700

    terraform fmt & add example vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,24 +5,24 @@ terraform {
 provider "google" {
   version = "2.15"
 
-#  project = "infra-271907"
+  #  project = "infra-271907"
   project = var.project
 
-#  region = "europe-west-1"
+  #  region = "europe-west-1"
   region = var.region
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-#  zone = "europe-west1-d"
+  #  zone = "europe-west1-d"
   zone = var.zone
   tags = ["reddit-app"]
 
   boot_disk {
     initialize_params {
-#      image = "reddit-base"
-#      image = "reddit-base-1585578160"
+      #      image = "reddit-base"
+      #      image = "reddit-base-1585578160"
       image = var.disk_image
     }
   }
@@ -33,21 +33,21 @@ resource "google_compute_instance" "app" {
   }
 
   metadata = {
-#    ssh-keys = "den_pirozhkov:${file("~/.ssh/id_rsa.pub")}"
+    #    ssh-keys = "den_pirozhkov:${file("~/.ssh/id_rsa.pub")}"
     ssh-keys = "den_pirozhkov:${file(var.public_key_path)}"
   }
 
   connection {
-    type = "ssh"
-    host = self.network_interface[0].access_config[0].nat_ip
-    user = "den_pirozhkov"
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "den_pirozhkov"
     agent = false
-#    private_key = file("~/.ssh/den_pirozhkov")
+    #    private_key = file("~/.ssh/den_pirozhkov")
     private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
@@ -58,14 +58,14 @@ resource "google_compute_instance" "app" {
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
+  target_tags   = ["reddit-app"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable project {
 }
 variable region {
   description = "Region"
-  default = "europe-west-1"
+  default     = "europe-west-1"
 }
 variable public_key_path {
   description = "Path to the public key used for ssh access"
@@ -16,5 +16,5 @@ variable private_key_path {
 }
 variable zone {
   description = "Zone"
-  default = "europe-west1-d"
+  default     = "europe-west1-d"
 }
