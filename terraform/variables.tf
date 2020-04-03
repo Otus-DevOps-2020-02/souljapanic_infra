089a3bf5f6edacca852edf7dc3b6ae226efe5317
souljapanic <souljapanic@gmail.com>
2020-04-04 01:57:23 +0700

    Add storage-bucket.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-d"
+}
+variable "public_keys" {
+  type        = list
+  description = "Pub Keys"
+}
+variable countIns {
+  description = "Count Instance"
+  default     = 1
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "ruby-base-1585913291"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "db-base-1585913708"
+}
